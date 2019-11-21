2586a8249d9d6fd6df79e8117a278c56c99df0b2
chieto <cuee26@gmail.com>
2019-11-21 23:05:58 +0100

    Added models for users

---
+++ Writer/models/UsersModel.js
@@ -0,0 +1,78 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+class Users {
+    constructor(firstName, lastName, email, gender, password, dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+        this.updatedAt = Date.now;
+    }
+
+    createSchema() {
+        return users = new Schema({
+            firstName: this.getFirstName(),
+            lastName: this.getLastName(),
+            email: this.getEmail(),
+            gender: this.getGender(),
+            password: this.getPassword(),
+            dateOfBirth: this.getDateOfBirth(),
+            updatedAt: Date.now
+        });
+    }
+}
+
+
+Users.prototype.setFirstName = function (firstName) {
+    this.firstName = firstName;
+}
+
+Users.prototype.getFirstName = function () {
+    return this.firstName;
+}
+
+Users.prototype.setLastName = function (lastName) {
+    this.lastName = lastName
+}
+
+Users.prototype.getLastName = function () {
+    return this.lastName;
+}
+
+
+Users.prototype.setEmail = function (email) {
+    this.email = email;
+}
+
+Users.prototype.getEmail = function () {
+    return this.email;
+}
+
+Users.prototype.setGender = function (gender) {
+    this.gender = gender;
+}
+
+Users.prototype.getGender = function () {
+    return this.gender;
+}
+
+Users.prototype.setDateOfBirth = function (dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+}
+
+Users.prototype.getDateOfBirth = function () {
+    return this.dateOfBirth;
+}
+
+Users.prototype.setPassword = function (password) {
+    this.password = password;
+}
+
+Users.prototype.getPassword = function () {
+    return this.password;
+}
+
+mongoose.model('UsersModel', new Users("firstName", "lastName", "test@test.com", "Male", "password", "30/01").createSchema());
